ed827a094042958fe3595bc5bfb15c34cd8f5357
Humaiza Naz <humaizaasghar@gmail.com>
2026-01-05 04:28:35 +0500

    Initial commit

---
+++ .next/static/chunks/src_app_page_tsx_445a3400._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_d07224ca._.js",
+  "static/chunks/src_7e7c7aad._.js"
+],
+    source: "dynamic"
+});
